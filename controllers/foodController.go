8614735bb5f7bd3e2828ba901f1a5483b569ef44
ChanchalS7 <chsverma7@gmail.com>
2024-08-30 23:56:18 +0530

    table-controller

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -111,9 +111,11 @@ func CreateFood() gin.HandlerFunc {
 	}
 }
 func round(num float64) int {
-
+	return int(num + math.Copysign(0.5,num))
 }
 func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output))/output
 }
 
 func UpdateFood() gin.HandlerFunc {
--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,6 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ChanchalS7/restaurant_management/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/genproto/googleapis/bigtable/admin/table/v1"
+)
+
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
 
 func GetTables() gin.HandlerFunc {
 	return func(*gin.Context) {
@@ -13,8 +26,36 @@ func GetTable() gin.HandlerFunc {
 	}
 }
 func CreateTable() gin.HandlerFunc {
-	return func(*gin.Context) {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		var table models.Table
+
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		validationErr := validate.Struct(table)
+
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+		table.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		table.ID = primitive.NewObjectID()
+		table.Table_id = table.ID.Hex()
+
+		result, inserErr := tableCollection.InsertOne(ctx, table)
+		if inserErr != nil {
+			msg := fmt.Sprintf("Table item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, result)
 	}
 }
 func UpdateTable() gin.HandlerFunc {
